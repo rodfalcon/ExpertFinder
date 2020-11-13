b42340ef6133c44c3e773c89211d3379ee069ae3
RafaMartins87 <rafael.martins87@outlook.com>
2020-11-12 21:57:19 -0300

    test post

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -37,10 +37,12 @@ namespace DatingApp.API.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Value>> Post([FromBody] string value)
         {
+            return await _mediator.Send(command);
+            //hello falcon! i dont know if i did right but at least you have a new pull request in your github rs
         }
-
+            
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
